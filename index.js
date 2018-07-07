2c8250e39d8605e8fadae45bcefa28cf802508ec
CapitalistLepton <CapitalistLepton@users.noreply.github.com>
2018-07-07 11:21:41 -0700

    Switch to handlebar for templates.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const cookie = require('cookie-parser');
+const hbs = require('hbs');
 const app = express();
 const AWS = require('aws-sdk');
 const Posts = require('./posts');
@@ -20,15 +21,33 @@ if (IS_OFFLINE === 'true') {
   dynamoDb = new AWS.DynamoDB.DocumentClient();
 }
 
+hbs.registerHelper('blurb', function(content) { return content.substr(0, 230) + '...' });
+hbs.registerHelper('caro', function(items, options) {
+  var out = '<div class="carousel-inner" role="listbox">';
+  for (var i = 0; i < items.length; i++) {
+    out += '<div class="carousel-item';
+    if (i == 0) {
+      out += ' active">';
+    } else {
+      out += '">';
+    }
+    out += options.fn(items[i]);
+  }
+  out += '</div>';
+  return out;
+});
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.use(bodyParser.json({ strict: false }));
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 app.use(cookie(process.env.COOKIE_SECRET));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 let bucket = process.env.S3_BUCKET; 
 
 app.get('/', function (req, res) {
+//  res.render('index', {bucket: bucket, id_token: req.signedCookies.id_token});
   Posts.index(req, res, bucket, dynamoDb);
 });
 
